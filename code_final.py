463488a3ee3b4573795cbdf85344da1824c7876e
rana-atfeh <rana.atfeh@servier.com>
2024-09-04 18:37:16 +0200

    download S3 files

--- code_final.py
+++ code_final.py
@@ -4,6 +4,7 @@ import joblib
 import spacy
 import os
 import re
+import boto3
 from spacy.cli import download
 from app.utils import load_spacy_model, process_text, predict_tags
 
@@ -14,26 +15,31 @@ mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI","http://15.237.111.188:5
 download("en_core_web_sm")
 nlp = load_spacy_model("en_core_web_sm")
 
-def download_artifact(run_id, artifact_path):
-    artifact = mlflow.artifacts.download_artifacts(run_id=run_id, artifact_path=artifact_path)
-    return artifact
+# Initialiser le client S3
+s3 = boto3.client('s3')
 
-adresse = "s3://mlflow-ratfeh/53ee1c48888743c28a5a733abe06a58f/artifacts/tfidf_vectorizer/vectorizer.pkl"
-vectorizer_path = mlflow.artifacts.download_artifacts(adresse)
-vectorizer = joblib.load(vectorizer_path)
+bucket_name = 'mlflow-ratfeh'
 
-# Chargement du binarizer :
-adresse = "s3://mlflow-ratfeh/53ee1c48888743c28a5a733abe06a58f/artifacts/binarizer/binarizer.pkl"
-binarizer_path = mlflow.artifacts.download_artifacts(adresse)
-binarizer = joblib.load(binarizer_path)
+vectorizer_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/tfidf_vectorizer/vectorizer.pkl'
+vectorizer_local_path = 'vectorizer.pkl'
+s3.download_file(bucket_name, vectorizer_key, vectorizer_local_path)
+vectorizer = joblib.load(vectorizer_local_path)
 
-# Chargement du modèle de classification :
-adresse = "s3://mlflow-ratfeh/53ee1c48888743c28a5a733abe06a58f/artifacts/model/model.pkl"
-model = mlflow.sklearn.load_model(adresse)
+# 2. Téléchargement et chargement du binarizer
+binarizer_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/binarizer/binarizer.pkl'
+binarizer_local_path = 'binarizer.pkl'
+s3.download_file(bucket_name, binarizer_key, binarizer_local_path)
+binarizer = joblib.load(binarizer_local_path)
+
+# 3. Téléchargement et chargement du modèle de classification
+model_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/model/model.pkl'
+model_local_path = 'model.pkl'
+s3.download_file(bucket_name, model_key, model_local_path)
+model = joblib.load(model_local_path)  
+
 
 # Titre de l'interface Streamlit :
 st.title('Classification de questions')
-
 
 # Champ de saisie du titre :
 titre = st.text_input('Entrer Titre :')
@@ -52,4 +58,3 @@ if st.button('Suggestion des Tags'):
     else:
         st.write('Veuillez saisir un titre ET une question.')
 
-        
