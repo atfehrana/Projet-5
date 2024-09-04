cd7993814de34f6cdc5830c4cca5ed7dfa57fe46
rana-atfeh <rana.atfeh@servier.com>
2024-09-04 19:25:35 +0200

    download S3 files

--- code_final.py
+++ code_final.py
@@ -13,28 +13,28 @@ mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI","http://15.237.111.188:5
 download("en_core_web_sm")
 nlp = load_spacy_model("en_core_web_sm")
 
-# Initialiser le client S3
 s3 = boto3.client('s3')
 bucket_name = 'mlflow-ratfeh'
 
-# Téléchargement et chargement du vectorizer
+# Téléchargement dans le répertoire /tmp sur Heroku
+vectorizer_local_path = '/tmp/vectorizer.pkl'
 vectorizer_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/tfidf_vectorizer/vectorizer.pkl'
-vectorizer_local_path = 'vectorizer.pkl'
+
+binarizer_local_path = '/tmp/binarizer.pkl'
+binarizer_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/binarizer/binarizer.pkl'
+
+model_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/model/model.pkl'
+model_local_path = '/tmp/model.pkl'
+
+# Téléchargez les fichiers S3 dans le répertoire temporaire
 s3.download_file(bucket_name, vectorizer_key, vectorizer_local_path)
 vectorizer = joblib.load(vectorizer_local_path)
 
-# Téléchargement et chargement du binarizer
-binarizer_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/binarizer/binarizer.pkl'
-binarizer_local_path = 'binarizer.pkl'
 s3.download_file(bucket_name, binarizer_key, binarizer_local_path)
 binarizer = joblib.load(binarizer_local_path)
 
-# Téléchargement et chargement du modèle de classification
-model_key = '53ee1c48888743c28a5a733abe06a58f/artifacts/model/model.pkl'
-model_local_path = 'model.pkl'
 s3.download_file(bucket_name, model_key, model_local_path)
-model = joblib.load(model_local_path)  
-
+model = joblib.load(model_local_path)
 
 # Titre de l'interface Streamlit :
 st.title('Classification de questions')
