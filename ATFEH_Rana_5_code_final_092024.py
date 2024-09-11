158e373a26675d074b15acec07acddab9d820042
rana-atfeh <rana.atfeh@servier.com>
2024-09-11 13:29:40 +0200

    Deploy API

--- ATFEH_Rana_5_code_final_092024.py
+++ ATFEH_Rana_5_code_final_092024.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import requests
 
+# Titre de l'interface Streamlit
 st.title('Classification de questions')
 
+# Champ de saisie du titre
 titre = st.text_input('Entrer Titre :')
+
+# Champ de saisie de la question
 question = st.text_input('Entrer Question :')
 
+# Bouton de prédiction
 if st.button('Suggestion des Tags'):
     if titre and question:
         # Appel à l'API Flask pour obtenir les tags prédits
-        api_url = "https://projet-5-14613fa14eef.herokuapp.com"  
+        api_url = "https://projet-5-14613fa14eef.herokuapp.com/predict" 
         payload = {'titre': titre, 'question': question}
         response = requests.post(api_url, json=payload)
 
--- api.py
+++ api.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
-from spacy.cli import download
 from app.utils import load_spacy_model, process_text
 
 app = Flask(__name__)
+CORS(app)  # Activer CORS pour toutes les routes
 
-download("en_core_web_sm")
+# Charger le modèle SpaCy
 nlp = load_spacy_model("en_core_web_sm")
 
+# Charger les artefacts de modèle
 vectorizer_local_path = './artefact/vectorizer.pkl'
 binarizer_local_path = './artefact/binarizer.pkl'
 model_local_path = './artefact/model.pkl'
@@ -19,18 +21,21 @@ model = joblib.load(model_local_path)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    titre = data.get('titre')
-    question = data.get('question')
 
-    if titre and question:
-        texte = process_text(nlp, titre + ' ' + question)
-        texte = ' '.join(texte)
-        vecteur = vectorizer.transform([texte])
-        prediction = model.predict(vecteur)
-        tags = binarizer.inverse_transform(prediction)
-        return jsonify({'tags_predits': tags[0]})
-    else:
-        return jsonify({'error': 'Titre et question manquants'}), 400
+    if not data or 'titre' not in data or 'question' not in data:
+        return jsonify({'error': 'Invalid data'}), 400
+
+    titre = data['titre']
+    question = data['question']
+
+    # Traiter le texte et faire la prédiction
+    texte = process_text(nlp, titre + ' ' + question)
+    texte = ' '.join(texte)
+    vecteur = vectorizer.transform([texte])
+    prediction = model.predict(vecteur)
+    tags = binarizer.inverse_transform(prediction)
+
+    return jsonify({'tags_predits': tags[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
