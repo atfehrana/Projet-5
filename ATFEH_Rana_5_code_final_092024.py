e53c8da0a11dfdf643c322063135a0301609078f
rana-atfeh <rana.atfeh@servier.com>
2024-09-18 14:43:07 +0200

    Updating API

--- ATFEH_Rana_5_code_final_092024.py
+++ ATFEH_Rana_5_code_final_092024.py
@@ -1,40 +1,27 @@
 import streamlit as st
-import joblib
-from spacy.cli import download
-from app.utils import load_spacy_model, process_text, predict_tags
+import requests
 
-# Chargement du modèle SpaCy:
-download("en_core_web_sm")
-nlp = load_spacy_model("en_core_web_sm")
-
-vectorizer_local_path = './artefact/vectorizer.pkl'
-binarizer_local_path = './artefact/binarizer.pkl'
-model_local_path = './artefact/model.pkl'
-
-# Charger les fichiers
-vectorizer = joblib.load(vectorizer_local_path)
-binarizer = joblib.load(binarizer_local_path)
-model = joblib.load(model_local_path)
-
-
-print("Tous les fichiers ont été chargés.")
-
-# Titre de l'interface Streamlit :
+# Titre de l'interface Streamlit
 st.title('Classification de questions')
 
-# Champ de saisie du titre :
+# Champ de saisie du titre
 titre = st.text_input('Entrer Titre :')
 
-# Champ de saisie de la question :
+# Champ de saisie de la question
 question = st.text_input('Entrer Question :')
 
 # Bouton de prédiction
 if st.button('Suggestion des Tags'):
     if titre and question:
-        texte = process_text(nlp, titre + ' ' + question)
-        texte = ' '.join(texte)
-        tags = predict_tags(vectorizer, binarizer, model, texte)
-        tags = tags[0].tolist()
-        st.write(f'Tags suggérés : {tags}')
+        # Utiliser l'URL de l'API Flask déployée sur Render
+        url = 'https://projet-5-oc.onrender.com/predict'
+        data = {'title': titre, 'question': question}
+        response = requests.post(url, json=data)
+        
+        if response.status_code == 200:
+            tags = response.json().get('suggested_tags', [])
+            st.write(f'Tags suggérés : {tags}')
+        else:
+            st.write('Une erreur est survenue lors de la prédiction.')
     else:
         st.write('Veuillez saisir un titre ET une question.')
---
+++ api.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from spacy.cli import download
+from app.utils import load_spacy_model, process_text, predict_tags
+
+app = Flask(__name__)
+
+# Chargement du modèle SpaCy:
+download("en_core_web_sm")
+nlp = load_spacy_model("en_core_web_sm")
+
+# Charger les fichiers
+vectorizer_local_path = './artefact/vectorizer.pkl'
+binarizer_local_path = './artefact/binarizer.pkl'
+model_local_path = './artefact/model.pkl'
+
+vectorizer = joblib.load(vectorizer_local_path)
+binarizer = joblib.load(binarizer_local_path)
+model = joblib.load(model_local_path)
+
+@app.route('/')
+def home_page():
+    return 'Bienvenue sur l\'API de suggestion de tags'
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    data = request.get_json(force=True)
+    title = data['title']
+    question = data['question']
+    if not title or not question:
+        return jsonify({'error': 'Titre et question sont requis.'})
+
+    texte = process_text(nlp, title + ' ' + question)
+    texte = ' '.join(texte)
+
+    tags = predict_tags(vectorizer, binarizer, model, texte)
+    tags = tags[0].tolist()
+
+    return jsonify({'suggested_tags': tags})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
